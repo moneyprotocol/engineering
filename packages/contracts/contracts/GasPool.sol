deaec2516098faca58234830af66207a369bcee6
B-Stable <nathan@braynechild.com>
2022-09-18 22:46:34 -0700

    Update comments

--- packages/contracts/contracts/GasPool.sol
+++ packages/contracts/contracts/GasPool.sol
@@ -5,13 +5,11 @@ pragma solidity 0.6.11;
 
 /**
  * The purpose of this contract is to hold BPD tokens for gas compensation:
- * https://github.com/moneyp/dev#gas-compensation
- * When a borrower opens a vault, an additional 50 BPD debt is issued,
- * and 50 BPD is minted and sent to this contract.
+ * When a borrower opens a vault, an additional 200 BPD debt is issued,
+ * and 200 BPD is minted and sent to this contract.
  * When a borrower closes their active vault, this gas compensation is refunded:
- * 50 BPD is burned from the this contract's balance, and the corresponding
- * 50 BPD debt on the vault is cancelled.
- * See this issue for more context: https://github.com/moneyp/dev/issues/186
+ * 200 BPD is burned from the this contract's balance, and the corresponding
+ * 200 BPD debt on the vault is cancelled.
  */
 contract GasPool {
     // do nothing, as the core contracts have permission to send to and burn from this address
